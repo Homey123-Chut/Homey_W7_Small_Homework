d4095f0b5b44385e8944cbbc8d92d12fdc34e846
Homey123-Chut <homey.chut@student.cadt.edu.kh>
2026-03-04 22:18:12 +0700

    Library View Model: Implement LibraryViewModel

--- ui/screens/library/view_model/library_view_model.dart
+++ ui/screens/library/view_model/library_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:practice/W7_small_Homework/model/songs/song.dart';
 import 'package:practice/W7_small_Homework/ui/states/settings_state.dart';
 
 import '../../../../data/repositories/songs/song_repository.dart';
@@ -9,10 +10,43 @@ class LibraryViewModel extends ChangeNotifier {
   final AppSettingsState settingsState;
   final PlayerState playerState;
 
+  List<Song> _songs = [];
+  List<Song> get songs => _songs;
+
+  Song? get currentSong => playerState.currentSong;
+
   LibraryViewModel({
     required this.songRepository,
     required this.settingsState,
     required this.playerState,
-  });
+  }) {
+    init();
+    playerState.addListener(_onPlayerStateChanged);
+  }
+
+    void init() {
+    _songs = songRepository.fetchSongs();
+    notifyListeners();
+  }
+
+  void _onPlayerStateChanged() {
+    notifyListeners();
+  }
+
+  void playSong(Song song) {
+    playerState.start(song);
+  }
+
+  void stopSong() {
+    playerState.stop();
+  }
+
+  bool isPlaying(Song song) => currentSong == song;
+
+  @override
+  void dispose() {
+    playerState.removeListener(_onPlayerStateChanged);
+    super.dispose();
+  }
 
 }
