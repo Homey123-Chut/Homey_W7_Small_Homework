78b1dfb7d825cdedad8cdd9cbd664a107945df67
Homey123-Chut <homey.chut@student.cadt.edu.kh>
2026-03-04 21:59:02 +0700

    Library View Model Create LibraryContent and LibraryViewModel

---
+++ ui/screens/library/view_model/library_view_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+import '../../../../data/repositories/songs/song_repository.dart';
+import '../../../../model/songs/song.dart';
+import '../../../states/player_state.dart';
+
+class LibraryViewModel extends ChangeNotifier {
+  
+}
